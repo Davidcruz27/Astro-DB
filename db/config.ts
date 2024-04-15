8c1b22ffdb9bc98ef3f9825e9d038f9cb9cddb23
David Cruz <dcstiven12@gmail.com>
2024-04-15 17:25:01 -0500

    proximo a corregir errores

--- db/config.ts
+++ db/config.ts
@@ -3,35 +3,35 @@ import { defineDb, defineTable, column } from "astro:db";
 // https://astro.build/db/config
 
 const User = defineTable({
-  columns: {
-    id: column.text({ primaryKey: true }),
-    email: column.text({ unique: true }),
-    username: column.text({ unique: true }),
-  },
+	columns: {
+		id: column.text({ primaryKey: true }),
+		email: column.text({ unique: true }),
+		username: column.text({ unique: true }),
+	},
 });
 
 const Todo = defineTable({
-  columns: {
-    id: column.text({ primaryKey: true }),
-    title: column.text(),
-    description: column.text(),
-    category: column.text(),
-    user_id: column.text({ references: () => User.columns.id }),
-    category_id: column.text({ references: () => Category.columns.id }),
-  },
+	columns: {
+		id: column.text({ primaryKey: true }),
+		title: column.text(),
+		description: column.text(),
+		category: column.text(),
+		user_id: column.text({ references: () => User.columns.id }),
+		category_id: column.text({ references: () => Category.columns.id }),
+	},
 });
 
 const Category = defineTable({
-  columns: {
-    id: column.text({ primaryKey: true }),
-    label: column.text({ unique: true }),
-  },
+	columns: {
+		id: column.text({ primaryKey: true }),
+		label: column.text({ unique: true }),
+	},
 });
 
 export default defineDb({
-  tables: {
-    User,
-    Todo,
-    Category,
-  },
+	tables: {
+		User,
+		Todo,
+		Category,
+	},
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,17 +1,45 @@
-import { db, User } from "astro:db";
+import { db, User, Todo, Category } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	await db.insert (User).values([
+	await db.insert(User).values([
 		{
 			id: "dus12",
 			email: "john.c.calhoun@examplepetstore.com",
 			username: "patricio",
 		},
 		{
-			id: "dus13",
+			id: "djhg13",
 			email: "juanlhoun@examplepetstore.com",
 			username: "juan",
 		},
+	]);
+
+	await db.insert(Category).values([
+		{
+			id: "kjaa145",
+			label: "Frameworks",
+		},
+		{
+			id: "hiufhf65",
+			label: "Marketing",
+		},
+	]);
+
+	await db.insert(Todo).values([
+		{
+			id: "hgft12",
+			title: "Crear funcion fecha",
+			description: "Crear funcion que adapte las fechas",
+			category_id: "kjaa145",
+			user_id: "dus12",
+		},
+		{
+			id: "hgft13",
+			title: "Crear anucio",
+			description: "Crear anuncio publicitario",
+			category_id: "hiufhf65",
+			user_id: "djhg13",
+		}
 	]);
 }
